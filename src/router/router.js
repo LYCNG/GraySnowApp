81bb0baa53a3dd4b541eec77b09d0bde7a55a814
fslynadmin <lyncheng@fs-technology.com>
2021-12-03 09:09:26 +0800

    loadable

--- src/router/router.js
+++ src/router/router.js
@@ -1,23 +1,24 @@
-import React,{ Suspense,lazy} from 'react'
+import React from 'react'
 import { Route,Switch } from "react-router-dom";
 // import {Home,LoginPage,News,AccountPage} from "../components"
+import loadable from '@loadable/component'
 import useTracker from '../hooks/useTracker';
 // const {Home,LoginPage,News,AccountPage}= lazy(()=>import("../components/index"));
-const Home = lazy(()=>import("../components/home"))
-const LoginPage = lazy(()=>import("../components/login"))
-const News = lazy(()=>import("../components/news"))
-const AccountPage = lazy(()=>import("../components/user"))
+const Home = loadable(()=>import("../components/home"))
+const LoginPage = loadable(()=>import("../components/login"))
+const News = loadable(()=>import("../components/news"))
+const AccountPage = loadable(()=>import("../components/user"))
 
 
 const BaseRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
+  
         <Switch>
             <Route exact path ={'/'} component={useTracker(Home) } />
             <Route exact path={"/login"} component={useTracker(LoginPage) } />
             <Route exact path={"/account"} component={useTracker(AccountPage) } />
             <Route exact path={"/news"} component={useTracker(News) } />
         </Switch>
-    </Suspense>
+
 
 );
 
