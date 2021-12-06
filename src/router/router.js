20f16f649df9b3ee5271c551a22ed5de202d9564
fslynadmin <lyncheng@fs-technology.com>
2021-12-06 08:58:15 +0800

    fix lazy

--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,22 @@
-import React,{Suspense,Fragment} from 'react'
+import React,{Suspense,Fragment,lazy} from 'react'
 import { Route,Switch } from "react-router-dom";
 // import {Home,LoginPage,News,AccountPage} from "../components"
 import loadable from '@loadable/component'
 import useTracker from '../hooks/useTracker';
 // const {Home,LoginPage,News,AccountPage}= lazy(()=>import("../components/index"));
-const Home = loadable(()=>import("../components/home"))
-const LoginPage = loadable(()=>import("../components/login"))
-const News = loadable(()=>import("../components/news"))
-const AccountPage = loadable(()=>import("../components/user"))
+// const Home = loadable(()=>import("../components/home"))
+// const LoginPage = loadable(()=>import("../components/login"))
+// const News = loadable(()=>import("../components/news"))
+// const AccountPage = loadable(()=>import("../components/user"))
+const Home = lazy(()=>import("../components/home"))
+const LoginPage = lazy(()=>import("../components/login"))
+const News = lazy(()=>import("../components/news"))
+const AccountPage = lazy(()=>import("../components/user"))
 
 
 const BaseRouter = () => (
         <Switch>
-            <Suspense fallback={<Fragment />}>
+            <Suspense fallback={<Fragment>Loading...</Fragment>}>
                 <Route exact path ={'/'} component={useTracker(Home) } />
                 <Route exact path={"/login"} component={useTracker(LoginPage) } />
                 <Route exact path={"/account"} component={useTracker(AccountPage) } />
